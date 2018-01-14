014b12bc3f5574d8816947c23d9e3ed2bd385c52
kanlav <35378218+kanlav@users.noreply.github.com>
2018-01-13 23:56:37 -0600

    Update pyTFlow1.py

--- pyTFlow1.py
+++ pyTFlow1.py
@@ -1,3 +1,12 @@
-import TensorFlow as tf
+# Import `tensorflow`
+import tensorflow as tf
 
-print(version(tf))
+# Initialize two constants
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+# Multiply
+result = tf.multiply(x1, x2)
+
+# Print the result
+print(result)
